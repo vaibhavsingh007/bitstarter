3f34013aab5a7d3cbf3ac51d383ec56206e4d0b6
Vaibhav Singh <vaibhav.singh21@yahoo.co.in>
2013-07-31 19:16:26 +0000

    test

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
-var buf = fs.readFileSync('/index.html');
+var buf = fs.readFileSync('/exdex.html');
 
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send("Hello World"); //indexBuf.toString('utf8', 0, buf.length)
+   response.send("Hello World"); //indexBuf.toString('utf8', 0, buf.length)
 });
 
 var port = process.env.PORT || 5000;
