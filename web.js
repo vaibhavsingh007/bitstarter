0dae9701cc88a0136d0b3b6168babf7371b29a57
Vaibhav Singh <vaibhav.singh21@yahoo.co.in>
2013-08-02 12:57:46 +0000

    updated web for absPath

--- web.js
+++ web.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
-var buf = fs.readFileSync('/exdex.html');
+//var buf = fs.readFileSync('index.html');
+var absPath = __dirname;
 
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-   response.send("Hello World"); //indexBuf.toString('utf8', 0, buf.length)
+   response.send(absPath.toString()); //indexBuf.toString('utf8', 0, buf.length)
 });
 
 var port = process.env.PORT || 5000;
