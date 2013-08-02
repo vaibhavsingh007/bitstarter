41d71a6211e5424aa56200eb0acc2554d9b2d64a
Vaibhav Singh <vaibhav.singh21@yahoo.co.in>
2013-08-02 13:52:43 +0000

    testing dir

--- web.js
+++ web.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var app = express();
+var fs = require("fs");
 var absPath = __dirname;
+var files = fs.readdirSync(absPath).toString();
 //var buf = fs.readFileSync(absPath + "/index.html");
 
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-   response.send(fs.readdirSync('.')); //indexBuf.toString('utf8', 0, buf.length));
+   response.send(files); //indexBuf.toString('utf8', 0, buf.length));
 });
 
 var port = process.env.PORT || 5000;
