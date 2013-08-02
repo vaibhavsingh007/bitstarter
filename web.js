60b464d2b0d9597bfdd4dab99a0d2154e1609ab3
Vaibhav Singh <vaibhav.singh21@yahoo.co.in>
2013-08-02 13:55:05 +0000

    testing index

--- web.js
+++ web.js
@@ -2,13 +2,13 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 var absPath = __dirname;
-var files = fs.readdirSync(absPath).toString();
-//var buf = fs.readFileSync(absPath + "/index.html");
+//var files = fs.readdirSync(absPath).toString();
+var buf = fs.readFileSync(absPath + "/index.html");
 
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-   response.send(files); //indexBuf.toString('utf8', 0, buf.length));
+   response.send(buf.toString('utf8', 0, buf.length));
 });
 
 var port = process.env.PORT || 5000;
